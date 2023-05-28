b35b875b9f2914668cd33cc7458a097153f26481
Michał <michals1099@gmail.com>
2023-05-28 12:04:51 +0200

    add json highlighting

--- src/app/client/ui/setup-info/setup-info.component.ts
+++ src/app/client/ui/setup-info/setup-info.component.ts
@@ -1,8 +1,18 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SocketSetup } from '../../data-access/models/socket-setup';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import * as Prism from 'prismjs';
+import 'prismjs/components/prism-json';
 
 @Component({
   selector: 'socketio-client-setup-info',
@@ -11,13 +21,19 @@ import { MatCardModule } from '@angular/material/card';
   templateUrl: './setup-info.component.html',
   styleUrls: ['./setup-info.component.scss'],
 })
-export class SetupInfoComponent {
+export class SetupInfoComponent implements AfterViewInit {
   @Input({ required: true })
   socketSetup!: SocketSetup;
   @Output()
   disconnectClicked: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @ViewChild('jsonCode')
+  jsonCode!: ElementRef;
 
   disconnect() {
     this.disconnectClicked.emit(true);
+  }
+
+  ngAfterViewInit() {
+    Prism.highlightElement(this.jsonCode.nativeElement);
   }
 }
